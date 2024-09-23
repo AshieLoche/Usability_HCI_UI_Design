0ae0430c09641ea8790f007752230f8a51085e91
Ashie Loche <134357122+AshieLoche@users.noreply.github.com>
2024-09-24 00:28:48 +0800

    Recreate Animation In Video - Update

--- Assets/Activities/RecreateAnimationInVideo/Scripts/GameManager.cs
+++ Assets/Activities/RecreateAnimationInVideo/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,18 @@ public class GameManager : MonoBehaviour
 {
 
     [SerializeField] private List<Transform> _markers;
+    [SerializeField] private List<GameObject> _cardDecks;
     [SerializeField] private List<GameObject> _blackCards;
     [SerializeField] private List<GameObject> _blueCards;
     [SerializeField] private List<GameObject> _redCards;
     [SerializeField] private Transform _card;
 
-    // Start is called before the first frame update
     void Start()
     {
+
         for (int i = 0; i < 5; i++)
         {
+
             GameObject card = Instantiate(_blackCards[Random.Range(0, _blackCards.Count)], _markers[i].position, Quaternion.Euler(new Vector3(0f, 0f, 180f)));
             card.transform.localScale = new Vector3(1f, 1f, 1f);
             card.transform.parent = _card;
@@ -24,7 +27,38 @@ public class GameManager : MonoBehaviour
             BoxCollider _boxCollider = card.GetComponent<BoxCollider>();
             _boxCollider.size = new Vector3(5.7f, 0.025f, 8.85f);
             _boxCollider.center = new Vector3(0f, 0f, 0f);
+
         }
+
+    }
+
+    private void Update()
+    {
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            foreach (GameObject cardDeck in _cardDecks)
+            {
+
+                if (hit.transform.gameObject == cardDeck)
+                {
+                    Material owo = cardDeck.GetComponent<Renderer>().material;
+                    owo.DOColor(Color.grey, 0.5f);
+                }
+                else
+                {
+                    Material owo = cardDeck.GetComponent<Renderer>().material;
+                    owo.DOColor(Color.white, 0.5f);
+                }
+
+            }
+
+        }
+
     }
 
 }
